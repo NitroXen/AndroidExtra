bec37a1f5c44586fe76dfa1b06a5dce22bbbe22f
Rubén Piñeiro Fernández <rubenpifeinstituto@gmail.com>
2024-12-16 12:40:58 +0100

    add_height

---
+++ app/src/main/java/com/nitroxen/androidextra/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.nitroxen.androidextra
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nitroxen.androidextra.calories.CaloriesActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalories:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        getInitialization()
+        getListeners()
+    }
+
+    private fun getInitialization() {
+        btnCalories = findViewById(R.id.btnCalories)
+    }
+
+    private fun getListeners(){
+        btnCalories.setOnClickListener {
+            val intent = Intent(this,CaloriesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
--- app/src/main/java/com/nitroxen/androidextra/calories/CaloriesActivity.kt
+++ app/src/main/java/com/nitroxen/androidextra/calories/CaloriesActivity.kt
@@ -1,26 +1,62 @@
 package com.nitroxen.androidextra.calories
 
+import java.text.DecimalFormat
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
 import com.nitroxen.androidextra.R
 
 class CaloriesActivity : AppCompatActivity() {
 
-    private lateinit var sGendre:SwitchCompat
+    private lateinit var sGender:SwitchCompat
+    private lateinit var rsHeight:RangeSlider
+    private lateinit var tvHeight:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_calories)
-
-
+        getInitialization()
+        getListeners()
+        getUI()
 
 
 
     }
+
+
+    private fun getInitialization() {
+        sGender = findViewById(R.id.sGender)
+        rsHeight = findViewById(R.id.rsHeight)
+        tvHeight = findViewById(R.id.tvHeight)
+
+    }
+
+    private fun getListeners() {
+        rsHeight.addOnChangeListener { _, value, _ ->
+            var result = setFormatedInt(value)
+            tvHeight.setText("$result cm")
+        }
+    }
+
+    private fun getUI() {
+
+    }
+
+    private fun setFormatedInt(valor:Float):String{
+        val df = DecimalFormat("#.##")
+        return df.format(valor)
+    }
+
+    private fun setFormatedDouble(valor:String):Double{
+        val df = DecimalFormat("#.##")
+        return df.format(valor).toDouble()
+    }
+
 }
