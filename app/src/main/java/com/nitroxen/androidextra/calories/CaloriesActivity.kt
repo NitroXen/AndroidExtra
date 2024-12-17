2122b4c1ea01c876c04bca714f35f23b32d434bd
Rubén Piñeiro Fernández <rubenpifeinstituto@gmail.com>
2024-12-17 10:49:06 +0100

    add weight and age

--- app/src/main/java/com/nitroxen/androidextra/calories/CaloriesActivity.kt
+++ app/src/main/java/com/nitroxen/androidextra/calories/CaloriesActivity.kt
@@ -7,15 +7,26 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.google.android.material.slider.Slider
 import com.nitroxen.androidextra.R
 
 class CaloriesActivity : AppCompatActivity() {
 
-    private lateinit var sGender:SwitchCompat
-    private lateinit var rsHeight:RangeSlider
-    private lateinit var tvHeight:TextView
+    private var weight: Int = 50
+    private var age: Int = 25
+    private var height : Int = 155
+
+    private lateinit var sGender: SwitchCompat
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var tvHeight: TextView
+    private lateinit var tvWeight: TextView
+    private lateinit var tvAge: TextView
+    private lateinit var fabSubstractWeight: FloatingActionButton
+    private lateinit var fabSubstractAge: FloatingActionButton
+    private lateinit var fabAddWeight: FloatingActionButton
+    private lateinit var fabAddAge: FloatingActionButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +38,6 @@ class CaloriesActivity : AppCompatActivity() {
         getUI()
 
 
-
     }
 
 
@@ -35,28 +45,84 @@ class CaloriesActivity : AppCompatActivity() {
         sGender = findViewById(R.id.sGender)
         rsHeight = findViewById(R.id.rsHeight)
         tvHeight = findViewById(R.id.tvHeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        tvAge = findViewById(R.id.tvAge)
+        fabSubstractWeight = findViewById(R.id.fabSubstractWeight)
+        fabSubstractAge = findViewById(R.id.fabSubstractAge)
+        fabAddWeight = findViewById(R.id.fabAddWeight)
+        fabAddAge = findViewById(R.id.fabAddAge)
 
     }
 
     private fun getListeners() {
         rsHeight.addOnChangeListener { _, value, _ ->
-            val result = setFormatedInt(value)
-            tvHeight.setText("$result cm")
+            height = setFormatedInt(value).toInt()
+            tvHeight.setText("$height cm")
+        }
+
+        fabSubstractAge.setOnClickListener {
+            modAge(false)
+        }
+        fabSubstractWeight.setOnClickListener {
+            modWeight(false)
+        }
+        fabAddWeight.setOnClickListener {
+            modWeight(true)
+        }
+        fabAddAge.setOnClickListener {
+            modAge(true)
         }
     }
 
     private fun getUI() {
+        tvHeight.text = "150 cm"
+        tvWeight.text = weight.toString()
+        tvAge.text = age.toString()
+        tvHeight.text = height.toString()
 
     }
 
-    private fun setFormatedInt(valor:Float):String{
+    private fun modWeight(add: Boolean) {
+        if (add) {
+            ++weight
+            tvWeight.text = weight.toString()
+            return
+        }
+        --weight
+        tvWeight.text = weight.toString()
+
+    }
+
+    private fun modAge(add: Boolean) {
+        if (add) {
+            ++age
+            tvAge.text = age.toString()
+            return
+        }
+        --age
+        tvAge.text = age.toString()
+
+    }
+
+    private fun setFormatedInt(valor: Float): String {
         val df = DecimalFormat("#.##")
         return df.format(valor)
     }
 
-    private fun setFormatedDouble(valor:String):Double{
+    private fun setFormatedDouble(valor: String): Double {
         val df = DecimalFormat("#.##")
         return df.format(valor).toDouble()
+    }
+
+    private fun calulateCalories():Double{
+        if(sGender.isActivated){
+           return (65+(9.6*weight)+(1.8*height)-(4.7*age))
+        }
+        return 66+(13.7*weight)+(5*height)-(6.8*age)
+    }
+
+    private fun calculateBMI():Int{
+        return weight/((height/100)*(height/100))
     }
 
 }
